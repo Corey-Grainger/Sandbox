c67d8783eaab2868dfcc0930d7068c2dd64039f3
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-10-03 12:08:34 +1000

    Add name remove names program

---
+++ week_04/remove_names.py
@@ -0,0 +1,17 @@
+"""Program to remove a name from a list of names"""
+
+
+names = ["Ada", "Alan", "Bill", "John"]
+
+print(", ".join(names))
+name_to_remove = input("Who do you want to remove? ").title()
+while name_to_remove != "":
+    try:
+        names.remove(name_to_remove)
+        print(f"You removed {name_to_remove}")
+    except ValueError:
+        print("Name not in names")
+    print(", ".join(names))
+    name_to_remove = input("Who do you want to remove? ").title()
+
+
