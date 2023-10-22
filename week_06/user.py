a3b022163fc24de7f8e6eba3ca2c4fb03cb30f3c
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-10-22 10:24:43 +1000

    Add user class for taco battles program

---
+++ week_06/user.py
@@ -0,0 +1,23 @@
+class User:
+    def __init__(self, name="", number_of_tacos=5, score=0):
+        self.name = name
+        self.number_of_tacos = number_of_tacos
+        self.score = score
+
+    def __repr__(self):
+        return (f"{self.name} Tacos: {self.number_of_tacos} Score: {self.score}")
+
+    def give_taco(self, recipient):
+        self.number_of_tacos -= 1
+        self.score += 1
+        recipient.get_taco()
+
+    def get_taco(self):
+        self.score += 1
+
+
+if __name__ == '__main__':
+    users = [User("Tim"), User("Michelle"), User("Lindsay")]
+    print(users)
+    users[0].give_taco(users[1])
+    print(users)
