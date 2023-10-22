ee209788e0929b1695814abbc25bf719b2e6fc16
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-10-22 11:12:26 +1000

    Add delete method to team class and testing for delete method to taco_battles program

--- week_06/taco_battles.py
+++ week_06/taco_battles.py
@@ -1,10 +1,26 @@
 """CP1404 Taco Battles Program"""
 
+from team import Team
+from user import User
+
 
 def main():
-    teams = [Team([User("Jim"), User("Bob"), User("Barry")], "Wildcats"), Team([User("Fred"), User("Ted"), User("Ned")], "Cowboys")]
+    teams = [Team([User("Jim"), User("Bob"), User("Barry")], "Wildcats"),
+             Team([User("Fred"), User("Ted"), User("Ned")], "Red Roos")]
     print(teams)
     for team in teams:
         print(f"{team.name}")
         for user in team.users:
             print(f"{user.name} has {user.number_of_tacos} Tacos and a score of {user.score}")
+    teams[0].add("Sam")
+    for team in teams:
+        print(f"{team.name}")
+        for user in team.users:
+            print(f"{user.name} has {user.number_of_tacos} Tacos and a score of {user.score}")
+    teams[0].delete("Sam")
+    for team in teams:
+        print(f"{team.name}")
+        for user in team.users:
+            print(f"{user.name} has {user.number_of_tacos} Tacos and a score of {user.score}")
+
+main()
--- week_06/team.py
+++ week_06/team.py
@@ -1,4 +1,5 @@
 from user import User
+from operator import attrgetter
 
 class Team:
     def __init__(self, users: list, name=""):
@@ -8,19 +9,10 @@ class Team:
     def __repr__(self):
         return f"Team {self.name}"
 
-    def add_user(self, name):
-        self.users.append(User(name))
+    def add(self, user_name):
+        self.users.append(User(user_name))
 
-
-if __name__ == '__main__':
-    teams = [Team([User("Jim"), User("Bob"), User("Barry")], "Wildcats"), Team([User("Fred"), User("Ted"), User("Ned")], "Cowboys")]
-    print(teams)
-    for team in teams:
-        print(f"{team.name}")
-        for user in team.users:
-            print(f"{user.name} has {user.number_of_tacos} Tacos and a score of {user.score}")
-    teams[0].add_user("Sam")
-    for team in teams:
-        print(f"{team.name}")
-        for user in team.users:
-            print(f"{user.name} has {user.number_of_tacos} Tacos and a score of {user.score}")
+    def delete(self, user_name: str):
+        for user in self.users:
+            if user.name == user_name:
+                self.users.remove(user)
